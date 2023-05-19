172ee0feb677c4a6d47626c6fec7fdd5a0efe927
Kenan Aydin <kenan.aydin@anywhere365.net>
2023-05-19 10:11:22 +0200

    some exception check unit tests are added

--- Application.Tests/CreateForecastCommandTests.cs
+++ Application.Tests/CreateForecastCommandTests.cs
@@ -6,14 +6,13 @@ using FluentAssertions;
 using static Application.Features.ForecastFeatures.Commands.CreateForecastCommand;
 using Application.Features.ForecastFeatures.Commands;
 using Shouldly;
+using Domain.Exceptions;
 
 namespace Application.Tests
 {
     public class CreateForecastCommandTests
     {
         private Mock<IWeatherForecastRepository> _weatherForecastRepository;
-
-
 
         [Fact]
         public async Task When_NoForecastAlreadyForDate_Then_ForecastIsAdded()
@@ -23,6 +22,33 @@ namespace Application.Tests
             var handler = new CreateForecastCommandHandler(_weatherForecastRepository.Object);
             var result = await handler.Handle(new CreateForecastCommand { ForDate = DateTime.Today, Temperature = 8 }, CancellationToken.None);
             result.ShouldBeOfType<int>();
+        }
+
+        [Fact]
+        public async Task When_TemperatureIsGreaterThan60_Then_ThrowWelcomeToHellException()
+        {
+            _weatherForecastRepository = new Mock<IWeatherForecastRepository>();
+            _weatherForecastRepository.Setup(x => x.GetWeatherForecastByDateAsync(It.IsAny<DateTime>())).Returns(Task.FromResult<WeatherForecast>(null));
+            var handler = new CreateForecastCommandHandler(_weatherForecastRepository.Object);
+            await Assert.ThrowsAsync<WelcomeToHellException>(() => handler.Handle(new CreateForecastCommand { ForDate = DateTime.Today, Temperature = 62 }, CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task When_TemperatureIsLessThanMinus60_Then_ThrowIceAgeException()
+        {
+            _weatherForecastRepository = new Mock<IWeatherForecastRepository>();
+            _weatherForecastRepository.Setup(x => x.GetWeatherForecastByDateAsync(It.IsAny<DateTime>())).Returns(Task.FromResult<WeatherForecast>(null));
+            var handler = new CreateForecastCommandHandler(_weatherForecastRepository.Object);
+            await Assert.ThrowsAsync<IceAgeException>(() => handler.Handle(new CreateForecastCommand { ForDate = DateTime.Today, Temperature = -80 }, CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task When_ForDateIsAlreadyPassed_Then_PastEntryException()
+        {
+            _weatherForecastRepository = new Mock<IWeatherForecastRepository>();
+            _weatherForecastRepository.Setup(x => x.GetWeatherForecastByDateAsync(It.IsAny<DateTime>())).Returns(Task.FromResult<WeatherForecast>(null));
+            var handler = new CreateForecastCommandHandler(_weatherForecastRepository.Object);
+            await Assert.ThrowsAsync<PastEntryException>(() => handler.Handle(new CreateForecastCommand { ForDate = DateTime.Today.AddDays(-2), Temperature = 32 }, CancellationToken.None));
         }
     }
 }
